2b999a69cd3e41dc390507b3d13383cad5dde685
Dinith <codse202f060@student.nibm.lk>
2023-06-06 11:05:07 +0530

    gradient card added

--- View/Home.jsx
+++ View/Home.jsx
@@ -1,6 +1,11 @@
-import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 import amashi from  '../assets/amashi.jpeg'
+import serach from  '../assets/search.png'
+import { Stack, Chip } from "@react-native-material/core";
+import { LinearGradient } from 'expo-linear-gradient';
+
+
 
 const Home = () => {
   return (
@@ -20,6 +25,8 @@ const Home = () => {
         <Text style={styles.Discover}>    
         Discover
         </Text>
+        <View style={styles.searchSection}>
+
         <TextInput 
         placeholder="Search..."
         placeholderTextColor="#fff" 
@@ -27,6 +34,30 @@ const Home = () => {
 
 
         </TextInput>
+        <Image  style={styles.searchIcon} source={serach}/>
+
+        </View>
+        <View>
+        <ScrollView   showsHorizontalScrollIndicator={false}
+ horizontal={true}>
+        <Stack style={styles.ChipStack} direction='row'  spacing={4}>
+    <Chip label="Trending"  style={styles.ActiveChip}  color='#fff'  />
+    <Chip variant="outlined"  style={styles.DisChip}  color='#fff' label="Action" />
+    <Chip variant="outlined"  style={styles.DisChip}  color='#fff' label="Strategy" />
+    <Chip variant="outlined"  style={styles.DisChip}  color='#fff' label="RPG" />
+    <Chip variant="outlined"  style={styles.DisChip}   color='#fff' label="Adventure" />
+
+  </Stack>
+  </ScrollView>
+  </View>
+
+  <View>
+  <LinearGradient colors={['#FC1518','#131313']} style={styles.GameCard}/>
+  <LinearGradient colors={['#E89340','#131313']} style={styles.GameCard}/>
+  <LinearGradient colors={['#E4E5E3','#131313']} style={styles.GameCard}/>
+
+  </View>
+
       
     </View>
   )
@@ -64,19 +95,56 @@ const styles = StyleSheet.create({
         marginLeft:10
 
     },
+    searchIcon: {
+        height:24,
+        width:24,
+        marginStart:20,
+
+
+    },
     Discover:{
         fontSize:34,    
         color:'#fff',
 
     },
     SearchBar:{
+        flex:1,
         width:'100%',
         height:40,
-        backgroundColor:'#232322',
         borderRadius:30,
-        marginTop:15,
-        paddingLeft:20
+        color:'#fff'
 
+    },
+    searchSection: {
+        flexDirection: 'row',
+        marginTop:15,
+        width:'100%',
+        backgroundColor:'#232322',
+        height:40,
+        borderRadius:30,
+        paddingHorizontal:20,
+        alignItems:'center'
+        
+        
+
+    },
+    ChipStack:{
+paddingTop:10,
+    },
+    ActiveChip:{
+        height:32,
+        backgroundColor:'#333333',
+
+
+    },
+    DisChip:{
+    },
+    GameCard:{
+        backgroundColor:'red',
+        width:'100%',
+        height:160,
+        marginVertical:20,
+        borderRadius:30,
     }
 
 })
